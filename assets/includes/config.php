fdcdd2b1c26c02425b80e82bf6ea150604fb87b8
thiagotgt3 <roll.brasil@gmail.com>
2023-10-31 20:56:54 -0300

    Add files via upload
    Signed-off-by: thiagotgt3 <roll.brasil@gmail.com>

---
+++ assets/includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbGM['host'] = 'b2dfvjalnkq2zicx31fl-mysql.services.clever-cloud.com';
+
+$dbGM['name'] = 'b2dfvjalnkq2zicx31fl';
+
+$dbGM['user'] = 'ubhd1jmrrokwdpo3';
+
+$dbGM['pass'] = 'Rn8t8CIhsoWrMhx4RpVu';
+
+$encryption = "vmbtrvw95105595885345**#3738s**A";
